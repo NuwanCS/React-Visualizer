89333c70e4feb03df0690bbc49d5b64dd9755e41
Nuwan Senevirathne <nuwan@neveroffside.com>
2024-01-28 20:54:42 +0530

    finalized animation

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -22,13 +22,12 @@ export const Canvas: React.FC<CanvasProps> = (props) => {
 
       for (let i = 1; i < len; i++) {
         if (arr[i - 1] > arr[i]) {
-          // Swap elements if they are in the wrong order
+          swapped = true;
           [arr[i - 1], arr[i]] = [arr[i], arr[i - 1]];
           trackedMoves.push({
             indicies: [i - 1, i],
             swap: true,
           });
-          swapped = true;
         } else {
           trackedMoves.push({
             indicies: [i - 1, i],
@@ -40,52 +39,47 @@ export const Canvas: React.FC<CanvasProps> = (props) => {
 
     return trackedMoves;
   };
+  const cols: Column[] = [];
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas?.getContext('2d');
 
-    const cols: Column[] = [];
     const n = props.array.length;
+
     if (canvas && context) {
+      const spacing = (canvas.width - margin * 2) / n;
+
+      for (let i = 0; i < props.array.length; i++) {
+        const x = i * spacing + spacing / 2 + margin;
+        const y = canvas.height - margin - i * 3;
+        const width = spacing - columnSpacing;
+        const height = (canvas.height - margin * 2) * props.array[i];
+        cols[i] = new Column(x, y, width, height);
+      }
+
       const animate = () => {
+        let changed = false;
         context.clearRect(0, 0, canvas.width, canvas.height);
         for (let i = 0; i < cols.length; i++) {
-          cols[i].draw(context);
+          changed = cols[i].draw(context) || changed;
         }
 
-        if (moves.length > 0) {
+        if (!changed && moves.length > 0) {
           const move = moves.shift();
           if (move) {
             const [i, j] = move.indicies;
-
-            console.log('i, j', i, j, cols[j]);
-
-            if (move?.swap) {
+            if (move.swap) {
               cols[i].moveToLocation(cols[j]);
-              cols[j].moveToLocation(cols[i]);
+              cols[j].moveToLocation(cols[i], -1);
               [cols[i], cols[j]] = [cols[j], cols[i]];
             } else {
               //todo
             }
           }
         }
-        requestAnimationFrame(animate);
+        requestAnimationFrame(() => animate());
       };
-      const spacing = (canvas.width - margin * 2) / n;
-
-      for (let i = 0; i < props.array.length - 1; i++) {
-        const x = i * spacing + spacing / 2 + margin;
-        const y = canvas.height - margin - i * 3;
-        const width = spacing - columnSpacing;
-        const height = (canvas.height - margin * 2) * props.array[i];
-        cols[i] = new Column(x, y, width, height);
-        // if () {
-        cols[i].draw(context);
-        // }
-      }
       moves = bubbleSort(props.array);
-      console.log(moves);
-
       animate();
     }
   }, [props.array, props.width, props.height]);
--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -4,43 +4,78 @@ export class Column {
     public y: number,
     public width: number,
     public height: number,
-    public queue: { x: number; y: number }[] = []
+    public queue: {
+      x: number;
+      y: number;
+      r: number;
+      g: number;
+      b: number;
+    }[] = [],
+    public color: {
+      r: number;
+      g: number;
+      b: number;
+    } = {
+      r: 197,
+      g: 197,
+      b: 197,
+    }
   ) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
     this.queue = [];
+    this.color = {
+      r: 197,
+      g: 197,
+      b: 197,
+    };
   }
 
   lerp(a: number, b: number, c: number) {
     return a + (b - a) * c;
   }
-  moveToLocation(location: { x: number; y: number }, frameCount = 100) {
-    //getting the all the inbetween values using linear interpolation
-    for (let i = 0; i < frameCount; i++) {
+  moveToLocation(
+    location: { x: number; y: number },
+    yOffest = 1,
+    frameCount = 30
+  ) {
+    //getting all the inbetween values using linear interpolation
+    for (let i = 1; i < frameCount; i++) {
       const t = i / frameCount;
+      const circularPath = Math.sin(t * Math.PI);
+
       this.queue.push({
         x: this.lerp(this.x, location.x, t),
-        y: this.lerp(this.y, location.y, t),
+        y:
+          this.lerp(this.y, location.y, t) +
+          ((circularPath * this.width) / 4) * yOffest,
+        r: this.lerp(150, 255, circularPath),
+        g: this.lerp(150, 0, circularPath),
+        b: this.lerp(150, 0, circularPath),
       });
     }
   }
 
   draw(ctx: CanvasRenderingContext2D) {
+    let changed = false;
     if (this.queue.length > 0) {
       const dequeuedItem = this.queue.shift();
       if (dequeuedItem !== undefined) {
-        const { x, y } = dequeuedItem;
+        const { x, y, r, g, b } = dequeuedItem;
         this.x = x;
         this.y = y;
+        this.color = { r, g, b };
+        changed = true;
       }
     }
     const left = this.x - this.width / 2;
     const top = this.y - this.height;
     const right = this.x + this.width / 2;
+    const { r, g, b } = this.color;
     ctx.beginPath();
-    ctx.fillStyle = '#c5c5c5';
+    ctx.fillStyle = `rgb(${r},${g},${b})`;
     ctx.moveTo(left, top);
     ctx.lineTo(left, this.y);
     ctx.ellipse(
@@ -67,5 +102,6 @@ export class Column {
     ctx.fill();
     ctx.stroke();
     // ctx.rect(left, top, this.width, this.height);
+    return changed;
   }
 }
